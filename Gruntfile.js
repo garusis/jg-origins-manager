d63a43597b10af2164c09a05692dc7c5f8b09260
Garusis <garusis@gmail.com>
2015-09-06 20:59:43 -0500

    Modificacion forzada para generar dist version

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,7 @@ module.exports = function (grunt) {
             }
         }
     });
+
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-clean');
